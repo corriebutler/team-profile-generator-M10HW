38deb21a3d098685b0e49d829d0eeeb7b97b2d9b
Corrie Butler <corriebutler12@gmail.com>
2021-11-21 16:57:04 -0700

    added tests for manager

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -4,28 +4,34 @@ const Employee = require('../lib/Employee');
 test('is employee an object?',() => {
     const e = new Employee ();
 
-    expect(typeof(e)).toBe('object')
+    expect(typeof(e)).toBe('object');
 });
 
 test('can we set the name?',() => {
     const name = 'Dave';
     const e = new Employee (name);
 
-    expect(e.name).toBe(name)
+    expect(e.name).toBe(name);
 });
 
 test('can we set the ID?',() => {
     const id = 5;
     const e = new Employee ('Dave', id);
 
-    expect(e.id).toBe(id)
+    expect(e.id).toBe(id);
 });
 
 test('can we set the email?',() => {
     const email = 'test@test.com';
     const e = new Employee ('Dave', 5, email);
 
-    expect(e.email).toBe(email)
+    expect(e.email).toBe(email);
+});
+
+test('does get role pull Employee',() => {
+    const e = new Employee();
+
+    expect(e.getRole()).toBe('Employee');
 });
 
 module.exports = Employee;
--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,6 +1,33 @@
-const { TestWatcher } = require('@jest/core');
 const Manager = require('../lib/Manager');
 
+test('is manager an object?',() => {
+    const manager = new Manager ();
+
+    expect(typeof(manager)).toBe('object');
+});
+
+
 test('is manager object created with info needed', () => {
-    const e = new Manager('Dave', '15', '12')
+    const name = 'Dave';
+    const id = 15;
+    const email = 'dave@dave.com';
+    const officeNumber = 12;
+    const manager = new Manager(name, id, email, officeNumber);
+
+    expect(manager.name).toBe(name);
+
+    expect(manager.id).toBe(id);
+    expect(manager.id).toEqual(expect.any(Number));
+
+    expect(manager.email).toBe(email);
+    expect(manager.email).toEqual(expect.stringContaining('@'));
+    
+    expect(manager.officeNumber).toBe(officeNumber);
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+});
+
+test('does get role pull Manager',() => {
+    const manager = new Manager();
+
+    expect(manager.getRole()).toBe('Manager');
 });
