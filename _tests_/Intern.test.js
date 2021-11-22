9eca2ae8442e791eaf4a3dfcf3608424158a64e8
Corrie Butler <corriebutler12@gmail.com>
2021-11-21 17:16:04 -0700

    added tests for intern

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -1,1 +1,32 @@
 const Intern = require('../lib/Intern');
+
+test('is intern an object?',() => {
+    const intern = new Intern ();
+
+    expect(typeof(intern)).toBe('object');
+});
+
+
+test('is intern object created with info needed', () => {
+    const name = 'John';
+    const id = 15;
+    const email = 'john@john.com';
+    const school = 'Yale';
+    const intern = new Intern(name, id, email, school);
+
+    expect(intern.name).toBe(name);
+
+    expect(intern.id).toBe(id);
+    expect(intern.id).toEqual(expect.any(Number));
+
+    expect(intern.email).toBe(email);
+    expect(intern.email).toEqual(expect.stringContaining('@'));
+    
+    expect(intern.school).toBe(school)
+});
+
+test('does get role pull Intern',() => {
+    const intern = new Intern();
+
+    expect(intern.getRole()).toBe('Intern');
+});
